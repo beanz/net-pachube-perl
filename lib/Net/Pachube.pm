9526bd004697fefdb8e029c965a3ca62f9b4e01f
Mark Hindess <soft-github@temporalanomaly.com>
2009-05-03 21:45:15 +0100

    Adding some documentation.

--- lib/Net/Pachube.pm
+++ lib/Net/Pachube.pm
@@ -1,71 +1,5 @@
 package Net::Pachube;
 
-use 5.006;
-use strict;
-use warnings;
-use Carp;
-use LWP::UserAgent;
-use HTTP::Request;
-use Net::Pachube::Response;
-
-require Exporter;
-our @ISA = qw(Exporter);
-our %EXPORT_TAGS = ( 'all' => [ qw() ] );
-our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
-our @EXPORT = qw();
-our $VERSION = '0.01';
-
-sub new {
-  my $pkg = shift;
-  bless {
-         pachube_url => 'http://www.pachube.com/api',
-         user_agent => LWP::UserAgent->new(),
-         key => $ENV{PACHUBE_API_KEY},
-         @_,
-        }, $pkg;
-}
-
-sub pachube_url {
-  $_[0]->{'pachube_url'} = $_[1] if (@_ > 1);
-  $_[0]->{'pachube_url'};
-}
-
-sub user_agent {
-  $_[0]->{'user_agent'} = $_[1] if (@_ > 1);
-  $_[0]->{'user_agent'};
-}
-
-sub key {
-  $_[0]->{'key'} = $_[1] if (@_ > 1);
-  $_[0]->{'key'};
-}
-
-sub feed {
-  $_[0]->{'feed'} = $_[1] if (@_ > 1);
-  $_[0]->{'feed'};
-}
-
-sub feed_url {
-  $_[0]->pachube_url.'/'.$_[0]->feed.'.xml';
-}
-
-sub get {
-  my ($self) = @_;
-  my $key = $self->key or
-    croak(q{No pachube api key defined.
-Set PACHUBE_API_KEY environment variable or pass 'key' parameter to the
-constructor.
-});
-  my $ua = $self->user_agent;
-  $ua->default_header('X-PachubeApiKey' => $key);
-  my $url = $self->feed_url;
-  my $request = HTTP::Request->new('GET' => $url);
-  Net::Pachube::Response->new(http_response => $ua->request($request));
-}
-
-1;
-__END__
-
 =head1 NAME
 
 Net::Pachube - Perl extension for manipulating pachube.com feeds
@@ -83,6 +17,150 @@ Net::Pachube - Perl extension for manipulating pachube.com feeds
 
 This module provides a simple API to fetch and/or update pachube.com
 feeds.
+
+=head1 METHODS
+
+=cut
+
+use 5.006;
+use strict;
+use warnings;
+use Carp;
+use LWP::UserAgent;
+use HTTP::Request;
+use Net::Pachube::Response;
+
+require Exporter;
+our @ISA = qw(Exporter);
+our %EXPORT_TAGS = ( 'all' => [ qw() ] );
+our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
+our @EXPORT = qw();
+our $VERSION = '0.01';
+
+=head2 C<new(%params)>
+
+The constructor creates a new L<Net:Pachube> object.  The constructor
+takes a parameter hash as arguments.  Valid parameters in the hash
+are:
+
+=over
+
+=item feed
+
+  The feed id to use.  This option is necessary for most methods to
+  work.
+
+=item key
+
+  The Pachube API key to use.  This parameter is optional.  If it is
+  not provided then the value of the environment variable
+  C<PACHUBE_API_KEY> is used.
+
+=item pachube_url
+
+  The base URL to use for all HTTP requests.  The default is
+  C<http://www.pachube.com/api>.
+
+=item user_agent
+
+  The L<LWP> user agent object to use for all HTTP requests.  The
+  default is to create a new one for each new L<Net::Pachube> object.
+
+=back
+
+=cut
+
+sub new {
+  my $pkg = shift;
+  bless {
+         pachube_url => 'http://www.pachube.com/api',
+         user_agent => LWP::UserAgent->new(),
+         key => $ENV{PACHUBE_API_KEY},
+         @_,
+        }, $pkg;
+}
+
+=head2 C<feed( [$new_feed] )>
+
+This method is an accessor/setter for the C<feed> attribute which is
+the feed id to use.
+
+=cut
+
+sub feed {
+  $_[0]->{'feed'} = $_[1] if (@_ > 1);
+  $_[0]->{'feed'};
+}
+
+=head2 C<key( [$new_key] )>
+
+This method is an accessor/setter for the C<key> attribute which is
+the Pachube API key to use.
+
+=cut
+
+sub key {
+  $_[0]->{'key'} = $_[1] if (@_ > 1);
+  $_[0]->{'key'};
+}
+
+=head2 C<pachube_url( [$new_url] )>
+
+This method is an accessor/setter for the C<pachube_url> attribute
+which is the base URL to to use for all HTTP requests.
+
+=cut
+
+sub pachube_url {
+  $_[0]->{'pachube_url'} = $_[1] if (@_ > 1);
+  $_[0]->{'pachube_url'};
+}
+
+=head2 C<user_agent( [$new_user_agent] )>
+
+This method is an accessor/setter for the C<user_agent> attribute
+which is the L<LWP> user agent object to use for all HTTP requests.
+
+=cut
+
+sub user_agent {
+  $_[0]->{'user_agent'} = $_[1] if (@_ > 1);
+  $_[0]->{'user_agent'};
+}
+
+=head2 C<feed_url( )>
+
+This method returns the URL of for the XML for the feed.
+
+=cut
+
+sub feed_url {
+  $_[0]->pachube_url.'/'.$_[0]->feed.'.xml';
+}
+
+=head2 C<get( )>
+
+This method returns a L<Net::Pachube::Response> object representing
+the result of attempting to obtain the data for the feed.
+
+=cut
+
+sub get {
+  my ($self) = @_;
+  my $key = $self->key or
+    croak(q{No pachube api key defined.
+Set PACHUBE_API_KEY environment variable or pass 'key' parameter to the
+constructor.
+});
+  my $ua = $self->user_agent;
+  $ua->default_header('X-PachubeApiKey' => $key);
+  my $url = $self->feed_url;
+  my $request = HTTP::Request->new('GET' => $url);
+  Net::Pachube::Response->new(http_response => $ua->request($request));
+}
+
+1;
+__END__
 
 =head2 EXPORT
 
--- lib/Net/Pachube/Response.pm
+++ lib/Net/Pachube/Response.pm
@@ -1,91 +1,5 @@
 package Net::Pachube::Response;
 
-use 5.006;
-use strict;
-use warnings;
-use Carp;
-use XML::Simple;
-
-require Exporter;
-our @ISA = qw(Exporter);
-our %EXPORT_TAGS = ( 'all' => [ qw() ] );
-our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
-our @EXPORT = qw();
-our $VERSION = '0.01';
-
-sub new {
-  my $pkg = shift;
-  bless {
-         @_,
-        }, $pkg;
-}
-
-sub http_response {
-  $_[0]->{'http_response'};
-}
-
-sub content {
-  $_[0]->http_response->content
-}
-
-sub is_success {
-  $_[0]->http_response->is_success
-}
-
-sub eeml {
-  $_[0]->{eeml} or $_[0]->{eeml} = XMLin($_[0]->content,
-                                         KeyAttr => [qw/id/],
-                                         ForceArray => [qw/data/]);
-}
-
-sub title {
-  $_[0]->eeml->{environment}->{title};
-}
-
-sub description {
-  $_[0]->eeml->{environment}->{description};
-}
-
-sub id {
-  $_[0]->eeml->{environment}->{id};
-}
-
-sub status {
-  $_[0]->eeml->{environment}->{status};
-}
-
-sub feed {
-  $_[0]->eeml->{environment}->{feed};
-}
-
-sub creator {
-  $_[0]->eeml->{environment}->{creator};
-}
-
-sub location {
-  defined $_[1] ? $_[0]->eeml->{environment}->{location}->{$_[1]} :
-    $_[0]->eeml->{environment}->{location};
-}
-
-sub data {
-  $_[0]->eeml->{environment}->{data}->{$_[1]||0}->{value}->{content};
-}
-
-sub data_min {
-  $_[0]->eeml->{environment}->{data}->{$_[1]||0}->{value}->{minValue};
-}
-
-sub data_max {
-  $_[0]->eeml->{environment}->{data}->{$_[1]||0}->{value}->{maxValue};
-}
-
-sub data_tag {
-  $_[0]->eeml->{environment}->{data}->{$_[1]||0}->{tag};
-}
-
-1;
-__END__
-
 =head1 NAME
 
 Net::Pachube::Response - Perl extension for manipulating pachube.com replies
@@ -100,8 +14,233 @@ Net::Pachube::Response - Perl extension for manipulating pachube.com replies
 
 =head1 DESCRIPTION
 
-This module provides a simple API to fetch and/or update pachube.com
-feeds.
+This module provides encapsulates the results of L<Net::Pachube>
+requests.
+
+=head1 METHODS
+
+=cut
+
+use 5.006;
+use strict;
+use warnings;
+use Carp;
+use XML::Simple;
+
+require Exporter;
+our @ISA = qw(Exporter);
+our %EXPORT_TAGS = ( 'all' => [ qw() ] );
+our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
+our @EXPORT = qw();
+our $VERSION = '0.01';
+
+=head2 C<new(%params)>
+
+The constructor creates a new L<Net:Pachube::Response> object.  This
+method is generally only called by the L<Net::Pachube> request
+methods.  The constructor takes a parameter hash as arguments.  Valid
+parameters in the hash are:
+
+=over
+
+=item http_response
+
+  The L<LWP> L<HTTP::Response> object for the L<Net::Pachube> request.
+
+=back
+
+=cut
+
+sub new {
+  my $pkg = shift;
+  bless {
+         @_,
+        }, $pkg;
+}
+
+=head2 C<http_response( )>
+
+This method is an accessor for the C<http_response> attribute which is
+the L<LWP> L<HTTP::Response> object for the L<Net::Pachube> request.
+
+=cut
+
+sub http_response {
+  $_[0]->{'http_response'};
+}
+
+=head2 C<content( )>
+
+This method is shorthand for calling the L<content> method on the
+L<http_request> attribute.  It returns the body of the HTTP response -
+typically some XML text.
+
+=cut
+
+sub content {
+  $_[0]->http_response->content
+}
+
+=head2 C<is_success( )>
+
+This method is shorthand for calling the L<is_success> method on the
+L<http_request> attribute.  It returns true if the request was
+successful.
+
+=cut
+
+sub is_success {
+  $_[0]->http_response->is_success
+}
+
+=head2 C<eeml( )>
+
+This method returns the L<EEML> from the body of the response if the
+request was successful.
+
+=cut
+
+sub eeml {
+  $_[0]->{eeml} or $_[0]->{eeml} = XMLin($_[0]->content,
+                                         KeyAttr => [qw/id/],
+                                         ForceArray => [qw/data/]);
+}
+
+=head2 C<title( )>
+
+This method returns the title of the feed from the L<EEML> if the
+request was successful.
+
+=cut
+
+sub title {
+  $_[0]->eeml->{environment}->{title};
+}
+
+=head2 C<description( )>
+
+This method returns the description of the feed from the L<EEML> if the
+request was successful.
+
+=cut
+
+sub description {
+  $_[0]->eeml->{environment}->{description};
+}
+
+=head2 C<id( )>
+
+This method returns the id of the feed from the L<EEML> if the request
+was successful.
+
+=cut
+
+sub id {
+  $_[0]->eeml->{environment}->{id};
+}
+
+=head2 C<status( )>
+
+This method returns the status of the feed from the L<EEML> if the request
+was successful.
+
+=cut
+
+sub status {
+  $_[0]->eeml->{environment}->{status};
+}
+
+=head2 C<feed( )>
+
+This method returns the URL for the feed from the L<EEML> if the
+request was successful.
+
+=cut
+
+sub feed {
+  $_[0]->eeml->{environment}->{feed};
+}
+
+=head2 C<creator( )>
+
+This method returns the creator value from the L<EEML> if the request
+was successful.
+
+=cut
+
+sub creator {
+  $_[0]->eeml->{environment}->{creator};
+}
+
+=head2 C<location( [ $key ] )>
+
+This method returns the location information from the L<EEML> if the
+request was successful.  If the optional C<key> parameter is not
+supplied then a hash reference will be returned.  If the optional
+C<key> parameter is supplied then the value for that key from the hash
+is returned.
+
+=cut
+
+sub location {
+  defined $_[1] ? $_[0]->eeml->{environment}->{location}->{$_[1]} :
+    $_[0]->eeml->{environment}->{location};
+}
+
+=head2 C<data( [ $index ] )>
+
+This method returns the value from the data element from the L<EEML>
+if the request was successful.  If the optional zero-based C<index>
+parameter is not provided, it is assumed to be zero.
+
+=cut
+
+sub data {
+  $_[0]->eeml->{environment}->{data}->{$_[1]||0}->{value}->{content};
+}
+
+=head2 C<data_min( [ $index ] )>
+
+This method returns the minimum value for the data element from the
+L<EEML> if the request was successful.  It may be undefined.  If the
+optional zero-based C<index> parameter is not provided, it is assumed
+to be zero.
+
+=cut
+
+sub data_min {
+  $_[0]->eeml->{environment}->{data}->{$_[1]||0}->{value}->{minValue};
+}
+
+=head2 C<data_max( [ $index ] )>
+
+This method returns the maximum value for the data element from the
+L<EEML> if the request was successful.  It may be undefined.  If the
+optional zero-based C<index> parameter is not provided, it is assumed
+to be zero.
+
+=cut
+
+sub data_max {
+  $_[0]->eeml->{environment}->{data}->{$_[1]||0}->{value}->{maxValue};
+}
+
+=head2 C<data_tag( [ $index ] )>
+
+This method returns the tag value for the data element from the
+L<EEML> if the request was successful.  It may be undefined, a
+singleton or a list reference if there are multiple tags.  If the
+optional zero-based C<index> parameter is not provided, it is assumed
+to be zero.
+
+=cut
+
+sub data_tag {
+  $_[0]->eeml->{environment}->{data}->{$_[1]||0}->{tag};
+}
+
+1;
+__END__
 
 =head2 EXPORT
 
