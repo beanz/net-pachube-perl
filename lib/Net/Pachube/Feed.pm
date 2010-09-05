426962f0ed6b8dad7518b3d666122e213096a19c
Mark Hindess <soft-github@temporalanomaly.com>
2010-09-05 20:03:54 +0100

    Fix POD bug: https://rt.cpan.org/Public/Bug/Display.html?id=51747.  Thx FWIE.

--- lib/Net/Pachube/Feed.pm
+++ lib/Net/Pachube/Feed.pm
@@ -115,7 +115,7 @@ sub description {
 =method C<feed_id( )>
 
 This method returns the id of the feed from the L<EEML> if the request
-was successful.  It should always be equal to C<$self->id> which is
+was successful.  It should always be equal to C<< $self->id >> which is
 used to request the feed data.
 
 =cut
@@ -234,7 +234,7 @@ sub data_tags {
   ref $tags ? @$tags : $tags
 }
 
-=method C<update( data => \@data_values )>
+=method C<<update( data => \@data_values )>>
 
 This method performs a C<PUT> request in order to update a feed.
 It returns true on success or undef otherwise.
